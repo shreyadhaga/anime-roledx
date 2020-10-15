a0dc51925b74bb9c54fd0596dffe70879b05b508
Shreya Dhaga <shreyadhaga@gmail.com>
2020-10-15 13:14:05 +0530

    changes

---
+++ build/precache-manifest.3a4325299ffc4bb5a431b223a69dffd1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cc91d62bff9c2da5b2202f6b53849cbf",
+    "url": "/anime-roledx/index.html"
+  },
+  {
+    "revision": "c711cf6067907a53c2fd",
+    "url": "/anime-roledx/static/css/main.bf3de13e.chunk.css"
+  },
+  {
+    "revision": "517b455b594f15b523b5",
+    "url": "/anime-roledx/static/js/2.9ada678b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/anime-roledx/static/js/2.9ada678b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c711cf6067907a53c2fd",
+    "url": "/anime-roledx/static/js/main.98c93d0b.chunk.js"
+  },
+  {
+    "revision": "6f1b9331a13f4ef38d5d",
+    "url": "/anime-roledx/static/js/runtime-main.e53d22d5.js"
+  }
+]);
